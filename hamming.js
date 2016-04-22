885c19dc70545e62f7e139f6a74e09eeb01d4da0
Yvan SRAKA <yvan.sraka@gmail.com>
2016-04-22 12:55:50 +0200

    Update JS skeleton file

--- hamming.js
+++ hamming.js
@@ -1,4 +1,12 @@
-function Hamming() {}
+//
+// This is only a SKELETON file for the 'Hamming' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+// Make sure to look at test.script.js--that should give you some hints about what is
+// expected here.
+
+'use strict';
+
+function Hamming() {};
 
 Hamming.prototype.compute = function (strand1, strand2) {
 //
@@ -7,4 +15,3 @@ Hamming.prototype.compute = function (strand1, strand2) {
 };
 
 module.exports = Hamming;
-
